7083f7e6bb1d4404ccdf3ba97d8bb344e6ecd3e0
Rc <ronaldocarvalho798@gmail.com>
2024-09-28 12:58:57 -0300

    close

--- java.js
+++ java.js
@@ -6,10 +6,6 @@ const linkkk = document.querySelector(".lkkk");
 const linkkkk = document.querySelector(".lkkkk");
 const servi = document.querySelector(".servi");
 
-
-
-
-
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 
 lin.addEventListener("click", () => nav.classList.toggle("active"));
@@ -47,44 +43,36 @@ const observery = new IntersectionObserver((entries) => {
   });
 });
 
-
 const hiddenElementsy = document.querySelectorAll(".hiddeny");
 hiddenElementsy.forEach((el) => observery.observe(el));
 
 function closeDescription(num) {
-  const descricao = document.getElementById('description' + num);
+  const descricao = document.getElementById("description" + num);
   const body = document.body;
 
-  descricao.style.display = 'none';
-  descricao.style.opacity = '0'; // Define a opacidade para 0 ao esconder
-  body.style.overflow = 'auto'; // Restaura o scroll
+  descricao.style.display = "none";
+  descricao.style.opacity = "0"; // Define a opacidade para 0 ao esconder
+  body.style.overflow = "auto"; // Restaura o scroll
 }
-
 
 function showDescription(num) {
-  const descricao = document.getElementById('description' + num);
+  const descricao = document.getElementById("description" + num);
   const body = document.body;
-  const video = descricao.querySelector('.meuVideo'); // Seleciona o vídeo pela classe
+  const video = descricao.querySelector(".meuVideo"); // Seleciona o vídeo pela classe
 
-
-   // Invertendo a lógica
-   if (descricao.style.display !== 'flex' && descricao.style.display !== 'block') {
+  // Invertendo a lógica
+  if (
+    descricao.style.display !== "flex" &&
+    descricao.style.display !== "block"
+  ) {
     // Mostra a descrição
-    descricao.style.display = 'flex';
-    descricao.style.opacity = '1'; // Define a opacidade para 1 ao mostrar
-    body.style.overflow = 'hidden'; // Desabilita o scroll
+    descricao.style.display = "flex";
+    descricao.style.opacity = "1"; // Define a opacidade para 1 ao mostrar
+    body.style.overflow = "hidden"; // Desabilita o scroll
     video.currentTime = 0; // Reinicia o vídeo
     video.play(); // Reproduz o vídeo
-} else {
+  } else {
     // Chama a função de fechar
     closeDescription(num);
+  }
 }
-
-}
-
-
-
-
-
-
-
