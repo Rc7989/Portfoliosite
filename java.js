c72c90f1aaa2b77b7c3023e1f4e5cdb3000329cc
Rc <ronaldocarvalho798@gmail.com>
2024-10-17 18:44:35 -0300

    spotlight

--- java.js
+++ java.js
@@ -76,3 +76,11 @@ function showDescription(num) {
     closeDescription(num);
   }
 }
+
+
+const updateCursor = ({ x, y }) => {
+  document.documentElement.style.setProperty('--x', x)
+  document.documentElement.style.setProperty('--y', y)
+}
+
+document.body.addEventListener('pointermove', updateCursor)
